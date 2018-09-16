9fb65b2f89acd1ead1a144248756255966282c73
SergioCarbone <sergio.carbone@gmail.com>
2018-09-16 14:18:17 -0300

    .

--- Ejercicios/ArraysYColecciones/ArraysYColecciones/Ejercicio_30/AutoF1.cs
+++ Ejercicios/ArraysYColecciones/ArraysYColecciones/Ejercicio_30/AutoF1.cs
@@ -8,5 +8,91 @@ namespace Ejercicio_30
 {
     public class AutoF1
     {
+        private short cantidadCombustible, numero, vueltasRestantes;
+        private bool enCompetencia;
+        private string escuderia;
+
+        public AutoF1(short numero, string escuderia)
+        {
+            this.cantidadCombustible = 0;
+            this.vueltasRestantes = 0;
+            this.enCompetencia = false;
+            this.numero = numero;
+            this.escuderia = escuderia;
+        }
+
+        /// <summary>
+        /// Muestra los datos del auto
+        /// </summary>
+        /// <returns></returns>
+        public string MostrarDatos()
+        {
+            StringBuilder datosAuto = new StringBuilder();
+
+            datosAuto.AppendFormat("Escuderia: {0}", this.escuderia);
+            datosAuto.AppendFormat("\nNumero: {0}", this.numero);
+            datosAuto.AppendFormat("\nEstado de competencia: {0}", this.enCompetencia);
+            datosAuto.AppendFormat("\nCombustible: {0}", this.cantidadCombustible);
+            datosAuto.AppendFormat("\nVueltas restantes: {0}", this.vueltasRestantes);
+
+            return datosAuto.ToString();
+        }
+
+        /// <summary>
+        /// Compara el numero y la escuderia de dos autos
+        /// </summary>
+        /// <param name="a1">auto a comparar</param>
+        /// <param name="a2">auto a comparar</param>
+        /// <returns>True si los autos son iguales, caso contrario false</returns>
+        public static bool operator ==(AutoF1 a1, AutoF1 a2)
+        {
+            bool retorno = false;
+            if(a1.escuderia == a2.escuderia && a1.numero == a2.numero)
+            {
+                retorno = true;
+            }
+            return retorno;
+        }
+
+        /// <summary>
+        /// Compara el numero y la escuderia de dos autos
+        /// </summary>
+        /// <param name="a1">auto a comparar</param>
+        /// <param name="a2">auto a comparar</param>
+        /// <returns>True si los autos son distintos, caso contrario false</returns>
+        public static bool operator !=(AutoF1 a1, AutoF1 a2)
+        {
+            return (!(a1 == a2));
+        }
+
+        public short GetCantidadCombustible()
+        {
+            return this.cantidadCombustible;
+        }
+
+        public void SetCantidadCombustible(short valor)
+        {
+            this.cantidadCombustible = valor;
+        }
+
+        public bool GetEnCompetencia()
+        {
+            return this.enCompetencia;
+        }
+
+        public void SetEnCompetencia(bool estado)
+        {
+            this.enCompetencia = estado;
+        }
+        
+        public short GetVueltas()
+        {
+            return this.vueltasRestantes;
+        }
+
+        public void SetVueltas(short vueltas)
+        {
+            this.vueltasRestantes = vueltas;
+        }
     }
 }
--- Ejercicios/ArraysYColecciones/ArraysYColecciones/Ejercicio_30/Program.cs
+++ Ejercicios/ArraysYColecciones/ArraysYColecciones/Ejercicio_30/Program.cs
@@ -10,7 +10,36 @@ namespace Ejercicio_30
     {
         static void Main(string[] args)
         {
+            Console.Title = "Ejercicio_ 30";
+            Console.ForegroundColor = ConsoleColor.Green;
 
-        }
+            Competencia monaco = new Competencia(15, 10);
+            AutoF1 a1 = new AutoF1(1, "ferrari");
+            AutoF1 a2 = new AutoF1(2, "ferrari");
+            AutoF1 a3 = new AutoF1(1, "mercedes");
+
+            if (monaco + a1)
+            {                
+                if(monaco + a2)
+                {
+                    if(monaco + a3)
+                    {
+                        Console.WriteLine(monaco.MostrarDatosCompetencia());
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Error");
+                }
+            }
+            Console.ReadKey();
+            Console.Clear();
+
+            if (monaco - a3)
+            {
+                Console.WriteLine(monaco.MostrarDatosCompetencia());
+            }
+            Console.ReadKey();
+        }        
     }
 }
