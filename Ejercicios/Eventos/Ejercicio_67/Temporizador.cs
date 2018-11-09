e682c4007faed019278c889d042b9469335e8af0
alumno <alumno>
2018-11-08 22:09:02 -0300

    .

---
+++ Ejercicios/Eventos/Ejercicio_67/Temporizador.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Ejercicio_67
+{
+    sealed class Temporizador
+    {
+        private Thread hilo;
+        private int intervalo;
+
+        public bool Activo
+        {
+            get
+            {
+                return this.hilo.IsAlive;
+            }
+            set
+            {
+                if(this.hilo.IsAlive == false || hilo != null)
+                {
+                    this.hilo.Abort();
+                }
+                else
+                {
+                    hilo = new Thread(Corriendo);
+                    hilo.Start();
+                }
+            }
+        }
+
+        public int Intervalo
+        {
+            get
+            {
+                return this.intervalo;
+            }
+        }
+
+        private void Corriendo()
+        {
+
+        }
+    }
+}
