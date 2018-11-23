3179018429fa2e3f4ae8afbafe8ac377d5c96824
alumno <alumno>
2018-11-22 21:14:59 -0300

    2018-11-22

---
+++ 20181122-SP/20181122-SP/Alumno/20181122-SP/FrmPpal.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Entidades;
+using Archivos;
+using System.Threading;
+
+namespace _20181122_SP
+{
+    public partial class FrmPpal : Form
+    {
+        List<Thread> listaHilos;
+        Queue<Patente> cola;
+        Thread hilo; 
+        public FrmPpal()
+        {
+            InitializeComponent();
+            this.listaHilos = new List<Thread>();
+            this.cola = new Queue<Patente>();
+        }
+
+        private void FrmPpal_Load(object sender, EventArgs e)
+        {
+            vistaPatente1.finExposicion += ProximaPatente;
+            vistaPatente2.finExposicion += ProximaPatente;
+        }
+
+
+        private void ProximaPatente(Patentes.VistaPatente vp)
+        {
+            hilo = new Thread(new ParameterizedThreadStart(vp.MostrarPatente));
+            hilo.Start();
+            listaHilos.Add(hilo);
+        }
+
+
+        private void FrmPpal_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            FinalizarSimulacion();
+        }
+        
+
+        private void btnXml_Click(object sender, EventArgs e)
+        {
+            Xml<string> xml = new Xml<string>();
+            //xml.Leer();
+        }
+
+        private void btnTxt_Click(object sender, EventArgs e)
+        {
+            Texto<string> texto = new Texto<string>();            
+            //texto.Leer(int.TryParse("patentes", out cola));
+            
+            
+        }
+
+        private void btnSql_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FinalizarSimulacion()
+        {
+            foreach (Thread aux in listaHilos)
+            {
+                if(aux.IsAlive == true)
+                {
+                    aux.Abort();
+                }
+            }
+        }
+    }
+}
---
+++ 20181122-SP/20181122-SP/Alumno/Archivos/Sql.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data;
+using Entidades;
+
+namespace Archivos
+{    
+    public class Sql : IArchivos<Patente>
+    {
+        SqlConnection conexion;
+        SqlCommand comando;
+        SqlDataReader dataReader;
+
+        public Sql()
+        {
+            string conectionStr = "Data Source=.\\SQLEXPRESS;Initial Catalog=final-20180802;Integrated Security=True";
+            conexion = new SqlConnection(conectionStr);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="tabla"></param>
+        /// <param name="datos"></param>
+        public void Guardar(string tabla, Queue<Patente> datos)
+        {
+            try
+            {
+                foreach (Patente p in datos)
+                {
+                    string query = string.Format("INSERT INTO '{0}' (patente,tipo) VALUES ('{1}','{2}')",
+                        tabla,p.CodigoPatente, p.TipoCodigo);
+                    Operar(query);
+                }                
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public void Leer(string tabla, out Queue<Patente> datos)
+        {
+            try
+            {
+                string query = string.Format("SELECT * FROM '{0}'", tabla);
+                comando = new SqlCommand(query, conexion);
+                conexion.Open();
+                dataReader = comando.ExecuteReader();
+                string patente;
+                Patente.Tipo tipo;
+                
+                while (dataReader.Read())
+                {
+                    patente = dataReader["patente"].ToString();
+                    tipo = (Patente.Tipo)dataReader["tipo"];
+                    Patente p = new Patente(patente, tipo);
+                    
+                }
+                
+                dataReader.Close();               
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                conexion.Close();
+            }
+        }
+
+
+
+        private void Operar(string query)
+        {
+            comando = new SqlCommand(query, conexion);
+            conexion.Open();
+            comando.ExecuteNonQuery();
+            conexion.Close();
+        }
+        
+    }
+}
